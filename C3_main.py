2c428baa73c374551a1484817c54f93edb3cb1be
JulesSucrot <51828364+JulesSucrot@users.noreply.github.com>
2023-03-26 23:48:17 +0200

    Update and rename main.py to C3_main.py

--- C3_main.py
+++ C3_main.py
@@ -1,7 +1,10 @@
-import fonction as fct
+import C3_fonction as fct
 
 fichier = input("Quel fichier souhaitez-vous utiliser  ? ")
 
 fichier = "C3-" + str(fichier) + ".txt"
 
 fct.Afficher(fichier)
+fct.deterministe(fichier)
+fct.complet(fichier)
+fct.standard(fichier)
